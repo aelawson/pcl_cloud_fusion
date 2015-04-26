06b5891949f1a3e920f4e087565baae64bb2cbd6
Andrew Lawson <aeriklawson@gmail.com>
2015-04-25 22:44:58 -0400

    Cleaned up code. Added function signatures for initial alignment.

--- registration.cpp
+++ registration.cpp
@@ -6,51 +6,64 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/registration/icp.h>
 #include <pcl/registration/icp_nl.h>
-    
+
+typedef pcl::PointCloud<pcl::PointXYZRGBA> KinectCloud;
+typedef pcl::PointXYZRGBA KinectPoint;
+
+// Filters a cloud using a Voxel Grid
+void filterCloud(pcl::VoxelGrid<KinectPoint> filter, KinectCloud cloud,
+    KinectCloud::Ptr cloudFiltered) {
+        filter.setInputCloud(cloud);
+        filter.filter(*cloudFiltered);
+}
+
+// Initially aligns two clouds using SAC
+void initialAlignment(KinectCloud cloudOne, KinectCloud cloudTwo) {
+
+}
+
+// Does second alignment of two clouds using ICP
+
+void finalAlignment(pcl::IterativeClosestPoint<KinectPoint, KinectPoint> icp,
+    KinectCloud cloudOne, KinectCloud cloudTwo, KinectCloud cloudAligned) {
+        icp.setInputSource(cloudTwo);
+        icp.setInputTarget(cloudOne);
+        icp.align(*cloudAligned);
+}
+
 int main () {
-    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_1 (new pcl::PointCloud<pcl::PointXYZRGBA>);
-    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_2 (new pcl::PointCloud<pcl::PointXYZRGBA>);
-    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_1_filtered (new pcl::PointCloud<pcl::PointXYZRGBA>);
-    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_2_filtered (new pcl::PointCloud<pcl::PointXYZRGBA>);
-    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_final (new pcl::PointCloud<pcl::PointXYZRGBA>);
-    pcl::io::loadPCDFile ("cloud_new_1.pcd", *cloud_1);
-    pcl::io::loadPCDFile ("cloud_new_2.pcd", *cloud_2);
+    // Declarations
+    KinectCloud::Ptr cloudOne (new KinectCloud);
+    KinectCloud::Ptr cloudTwo (new KinectCloud);
+    KinectCloud::Ptr cloudOneFiltered (new KinectCloud);
+    KinectCloud::Ptr cloudTwoFiltered (new KinectCloud);
+    KinectCloud::Ptr cloudAligned (new KinectCloud);
+    pcl::VoxelGrid<KinectPoint> voxel;
+    pcl::IterativeClosestPoint<KinectPoint, KinectPoint> icp;
 
-    // Filtering
-    pcl::VoxelGrid<pcl::PointXYZRGBA> voxel;
-    voxel.setInputCloud(cloud_1);
+    // Parameterizations
     voxel.setLeafSize(0.05f, 0.05f, 0.05f);
-    voxel.filter(*cloud_1_filtered);
-
-    voxel.setInputCloud(cloud_2);
-    voxel.setLeafSize(0.05f, 0.05f, 0.05f);
-    voxel.filter(*cloud_2_filtered);
-
-    // Registration
-    pcl::IterativeClosestPoint<pcl::PointXYZRGBA, pcl::PointXYZRGBA> icp;
     icp.setMaxCorrespondenceDistance (50);
     icp.setMaximumIterations (50);
     icp.setTransformationEpsilon (1e-8);
     icp.setEuclideanFitnessEpsilon (1);
-    icp.setInputSource(cloud_2_filtered);
-    icp.setInputTarget(cloud_1_filtered);
-    icp.align(*cloud_final);
+    pcl::io::loadPCDFile ("cloud_new_1.pcd", *cloudOne);
+    pcl::io::loadPCDFile ("cloud_new_2.pcd", *cloudTwo);
 
-    //pcl::IterativeClosestPointNonLinear<pcl::PointXYZRGBA, pcl::PointXYZRGBA> reg;
-    //reg.setTransformationEpsilon(1e-6);
-    //reg.setMaxCorrespondenceDistance(0.1);
-    //reg.setMaximumIterations(30);
-    //reg.setInputSource(cloud_2_filtered);
-    //reg.setInputTarget(cloud_1_filtered);
-    //reg.align(*cloud_final);
+    // Filtering
+    filterCloud(voxel, cloudOne, cloudOneFiltered);
+    filterCloud(voxel, cloudTwo, cloudTwoFiltered);
+
+    // Registration
+    finalAlignment(cloudOneFiltered, cloudTwoFiltered, cloudAligned);
 
     // Cloud concatenation
-    *cloud_1_filtered += *cloud_final;
+    *cloudOneFiltered += *cloudAligned;
 
     pcl::visualization::CloudViewer viewer("Cloud Viewer");
-    
+
     // Blocks until the cloud is actually rendered
-    viewer.showCloud(cloud_1_filtered);
+    viewer.showCloud(cloudOneFiltered);
     while (!viewer.wasStopped ()) {
     }
     return 0;
