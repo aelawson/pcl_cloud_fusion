163a6b9a779790c0beea76dbe85d224f2cff1c14
Andrew Lawson <aeriklawson@gmail.com>
2015-05-13 18:17:31 -0400

    Testing time differnece.

--- src/cloud_fusion.cpp
+++ src/cloud_fusion.cpp
@@ -143,6 +143,8 @@ void streamCallbackRobot1(const sensor_msgs::PointCloud2& cloudRos) {
     Eigen::Affine3d transformEigen;
     try {
         tfListener.waitForTransform(fixedFrame, cloudFrame, ros::Time(0), ros::Duration(10.0));
+        ROS_INFO("Cloud stamp is: %d", (int) cloudFrame.header.stamp.toSec());
+        ROS_INFO("ROS time is: %d", (int) ros::Time(0));
         tfListener.lookupTransform(fixedFrame, cloudFrame, ros::Time(0), transform);
         tf::transformTFToEigen(transform, transformEigen);
         pcl::transformPointCloud(*cloudNew, *cloudTransf, transformEigen);
