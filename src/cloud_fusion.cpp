14285329b1fbca78a4a785e06cbae07e05ca294c
Andrew Lawson <aeriklawson@gmail.com>
2015-05-13 17:32:10 -0400

    Removed waiting.

--- src/cloud_fusion.cpp
+++ src/cloud_fusion.cpp
@@ -138,12 +138,11 @@ void streamCallbackRobot1(const sensor_msgs::PointCloud2& cloudRos) {
     ROS_INFO("Cloud frame id is: %s", cloudFrame.c_str());
 
     // Get and apply transform from camera to map
-    tf::TransformListener tfListener(ros::Duration(50.0), true);
+    tf::TransformListener tfListener(ros::Duration(60.0), true);
     tf::StampedTransform transform;
     Eigen::Affine3d transformEigen;
     try {
-        tfListener.waitForTransform(fixedFrame, cloudFrame, ros::Time((double) cloudRos.header.stamp.toSec()), ros::Duration(20.0));
-        tfListener.lookupTransform(fixedFrame, cloudFrame, ros::Time((double) cloudRos.header.stamp.toSec()), transform);
+        tfListener.lookupTransform(fixedFrame, cloudFrame, ros::Time((double) cloudRos.header.stamp), transform);
         tf::transformTFToEigen(transform, transformEigen);
         pcl::transformPointCloud(*cloudNew, *cloudTransf, transformEigen);
     }
