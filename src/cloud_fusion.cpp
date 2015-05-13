cec359e8559e89dd850a764113ef27f874670e8e
Andrew Lawson <aeriklawson@gmail.com>
2015-05-13 15:30:49 -0400

    Increased to one second.

--- src/cloud_fusion.cpp
+++ src/cloud_fusion.cpp
@@ -142,7 +142,7 @@ void streamCallbackRobot1(const sensor_msgs::PointCloud2& cloudRos) {
     tf::StampedTransform transform;
     Eigen::Affine3d transformEigen;
     try {
-        tfListener.waitForTransform(fixedFrame, cloudFrame, ros::Time(0), ros::Duration(0.5));
+        tfListener.waitForTransform(fixedFrame, cloudFrame, ros::Time(0), ros::Duration(1.0));
         tfListener.lookupTransform(fixedFrame, cloudFrame, ros::Time(0), transform);
         tf::transformTFToEigen(transform, transformEigen);
         pcl::transformPointCloud(*cloudNew, *cloudTransf, transformEigen);
